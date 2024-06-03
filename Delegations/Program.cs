61eb29e351d39e989c0eb27399fa1bbc2d936ae2
Omer MEMES <oms20x@gmail.com>
2024-06-03 18:13:24 +0300

    Add project files.

---
+++ Delegations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsSimpleDelegate.SubscribeToDelegates();
+            clsSimpleDelegate.CallFunctions1();
+            clsSimpleDelegate.UnsubscribeToDelegates();
+            clsSimpleDelegate.CallFunction2();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Delegations/clsSimpleDelegate.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+
+
+public class clsSimpleDelegate
+{
+    private delegate void CallFunction();
+    static CallFunction Caller;
+    private static void _Print()
+    {
+        Console.WriteLine("hello world!");
+    }
+
+    private static void _Print2()
+    {
+        Console.WriteLine("this is a print2 fuction");
+    }
+    public static void SubscribeToDelegates()
+    {
+        Caller += _Print;
+        Caller += _Print2;
+        Caller += () => { Console.WriteLine("this is an anonymous function"); };
+    }
+    public static void UnsubscribeToDelegates()
+    {
+        Caller -= _Print;
+        Caller -= _Print2;
+        Caller -= () => { Console.WriteLine("this is an anonymous function"); };
+    }
+    public static void CallFunctions1()
+    {
+        Console.WriteLine("\nCalled Functions");
+        Caller?.Invoke();
+    }
+
+    public static void CallFunction2()
+    {
+        Console.WriteLine("\nCalled Functions");
+        if (Caller != null)
+        {
+            Caller();
+        }
+
+        /*
+         Called Functions
+         hello world!
+         this is a print2 fuction
+         this is an anonymous function
+         
+         Called Functions
+         this is an anonymous function
+        */
+    }
+}

4786826391bd330cc8791f8cf045db05b4fda103
Ömer MEMES <omer.memes20@gmail.com>
2024-06-05 08:32:08 +0300

    publisher subscriber design pattern example 2 (to do)

---
+++ PublisherSubscriberDesignPattern/clsExample2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class NewArticle
+{
+    public string Title { get; set; }   
+    public string Description { get; set; }
+    public string Author { get; set; }
+
+    public NewArticle(string Author, string Title, string Decription)
+    {
+        this.Author = Author;
+        this.Title = Title;
+        this.Description = Decription;
+    }
+}
+

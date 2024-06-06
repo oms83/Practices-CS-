da062f7894b2e90bc440f6d0efa766447ed71e02
Ömer MEMES <omer.memes20@gmail.com>
2024-06-06 12:24:23 +0300

    Func delegate example 1

---
+++ Func Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Func_Delegates
+{
+    internal class Program
+    {
+        public static void FunctionwithNoParameters()
+        {
+            Func<string> getGreeting = () => "Hello, world";
+            Console.WriteLine(getGreeting());
+        }
+
+        static void Main(string[] args)
+        {
+            FunctionwithNoParameters();
+
+            Console.ReadKey();
+        }
+    }
+}

e4cf0e93bba177b9038753ddb6dd059dd4bae20e
Ömer MEMES <omer.memes20@gmail.com>
2024-06-06 13:28:59 +0300

    Difinition of Predicate Delegate

---
+++ Predicate Delegate/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// the Predicate delegate in C#. The Predicate delegate represents a method that takes a single
+// parameter and returns a boolean value, typically used for testing conditions.
+
+namespace Predicate_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

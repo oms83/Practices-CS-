69b6b20ad4032c600996799f194859636320e333
Ömer MEMES <omer.memes20@gmail.com>
2024-06-06 19:59:07 +0300

    Definition of Action Delegate

---
+++ Action Delegate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+    An Action delegate in C# is a predefined delegate type provided by the .NET Framework that 
+    represents a method that performs an action but does not return a value. It can encapsulate 
+    a method that takes zero or more parameters but always has a void return type. There are 
+    different variations of the Action delegate, which can take up to 16 parameters. 
+*/
+
+namespace Action_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

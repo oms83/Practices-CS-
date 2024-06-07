c5ea437f4ce9ae84c53c637a01900028d517c728
Ömer MEMES <omer.memes20@gmail.com>
2024-06-07 15:45:17 +0300

    Nullable Data Type Example 1

---
+++ Nullable Data Types/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Example1()
+    {
+        Nullable<int> age = new Nullable<int>();
+        Console.WriteLine(age); // null
+        age = 23;
+        Console.WriteLine(age);
+
+    }
+
+    static void Main(string[] args)
+    {
+        Example1();
+
+        Console.ReadKey();
+    }
+}
+

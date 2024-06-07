278456852db5774c9484c96fc02ef7baea724378
Ömer MEMES <omer.memes20@gmail.com>
2024-06-07 17:39:38 +0300

    Custom Attribute Example 1

---
+++ Reflection with Custom Attributes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public class MyCustomAttribute : Attribute
+{
+    public string Description { get; }
+
+    public MyCustomAttribute(string description)
+    {
+        Description = description;
+    }
+}
+
+[MyCustom("This is a class attribute")]
+public class MyClass
+{
+    [MyCustom("This is a method attribute")]
+    public void MyMethod()
+    {
+
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Type classInfo = typeof(MyClass);
+        object[] classAttributes = classInfo.GetCustomAttributes(typeof(MyCustomAttribute), false);
+
+        foreach (MyCustomAttribute attribute in classAttributes)
+        {
+            Console.WriteLine(attribute.Description);
+        }
+
+        MethodInfo methodInfo = classInfo.GetMethod("MyMethod");
+        object[] methodAttributes = methodInfo.GetCustomAttributes(typeof (MyCustomAttribute), false);
+        foreach (MyCustomAttribute attribute in methodAttributes)
+        {
+            Console.WriteLine(attribute.Description);
+        }
+
+        Console.ReadKey();
+    }
+}

0ae87f7bf2637cb8e3fd94cd2ef2982f0c8c6be8
Ömer MEMES <omer.memes20@gmail.com>
2024-06-08 10:49:55 +0300

    Parallel.For

---
+++ Asynchronous Programming Parallel Class/Parallel_for.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+internal class Parallel_for
+{
+    public static void run()
+    {
+        int numberOfIterations = 10;
+
+        Parallel.For(0, numberOfIterations, i => {
+            Console.WriteLine($"Execution iteration {i} on thread {Task.CurrentId}");
+            Task.Delay(1000).Wait();
+        });
+
+        Console.WriteLine("All iterations completed.");
+
+        Parallel.For(0, numberOfIterations, ProcessIteration);
+        Console.WriteLine("All iterations completed.");
+    }
+
+    public static void ProcessIteration(int i)
+    {
+        Console.WriteLine($"Execution iteration {i} on thread {Task.CurrentId}");
+        Task.Delay(1000).Wait();
+    }
+}
---
+++ Asynchronous Programming Parallel Class/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asynchronous_Programming_Parallel_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel_for.run();
+            Console.ReadKey();
+        }
+    }
+}
